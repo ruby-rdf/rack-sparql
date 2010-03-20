a869a5a6c2e4d8333b089ead0cdfb803738b9aaf
Arto Bendiken <arto@bendiken.net>
2010-03-20 22:26:29 +0100

    Implemented Rack::SPARQL::VERSION.to_a.

--- lib/rack/sparql/version.rb
+++ lib/rack/sparql/version.rb
@@ -10,10 +10,14 @@ module Rack module SPARQL
 
     ##
     # @return [String]
-    def self.to_s()   STRING end
+    def self.to_s() STRING end
 
     ##
     # @return [String]
     def self.to_str() STRING end
+
+    ##
+    # @return [Array(Integer, Integer, Integer)]
+    def self.to_a() [MAJOR, MINOR, TINY] end
   end
 end end
