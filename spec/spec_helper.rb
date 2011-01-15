14fd3ffc92151187a124eef61e1c799af9d9767b
Arto Bendiken <arto@bendiken.net>
2011-01-15 20:21:20 +0100

    Improved the spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,18 @@ require 'rack/test'
 require 'rdf/spec'
 
 RSpec.configure do |config|
-  config.include(RDF::Spec::Matchers)
+  config.include RDF::Spec::Matchers
+
+  config.mock_with :rspec
+  config.include Rack::Test::Methods
+
+  def not_found
+    mock('404 Not Found app', :call => [404, {}, ["Not Found"]])
+  end
+
+  def app
+    Rack::SPARQL::Endpoint.new(not_found, Rack::SPARQL::Server.new)
+  end
 end
 
 include Rack::SPARQL
