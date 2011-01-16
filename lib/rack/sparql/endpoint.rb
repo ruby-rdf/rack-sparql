6f8e3614c11184911dab4c9d000050659e276190
Arto Bendiken <arto@bendiken.net>
2011-01-16 01:30:17 +0100

    Added an Endpoint#server accessor.

--- lib/rack/sparql/endpoint.rb
+++ lib/rack/sparql/endpoint.rb
@@ -9,6 +9,22 @@ module Rack::SPARQL
     REQUEST_METHODS    = %w(GET POST)
     INPUT_CONTENT_TYPE = 'application/x-www-form-urlencoded'
 
+    # @return [Server]
+    attr_reader :server
+    def server
+      @server = @server.call if @server.is_a?(Proc)
+      @server
+    end
+
+    ##
+    # @param  [#call] app
+    # @param  [Server] server
+    # @param  [Hash] options
+    def initialize(app, server, options = {})
+      @server = server
+      super(app, options)
+    end
+
     ##
     # @private
     # @see Rack::SPARQL::Middleware#call
